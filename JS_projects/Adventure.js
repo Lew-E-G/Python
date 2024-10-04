3dbefb3f59158e668ab942e393fc2feeea119ce4
Lewis <luigizzle@gmail.com>
2024-10-04 21:40:44 +0200

    Using functions to make things more modular

--- JS_projects/Adventure.js
+++ JS_projects/Adventure.js
@@ -2,21 +2,29 @@ const prompt = require("prompt-sync")() //creating a varibale for fetching pf us
 const name = prompt("Please enter your name: ")
 console.log("Welcome",name)
 
-const shouldWePlay = prompt("Would you like to play Lewis' Life Lessons?")
-const condition = shouldWePlay.toLowerCase() === "yes"
+startPlay()     //where the game initially starts
 
-if (shouldWePlay.toLowerCase() === "yes") {
-    console.log("Great, please make youre choice by typing either 'left' or 'right'")
-    const firstTurn = prompt("You are hiking Ben Nevis with some friends, and you come to a fork in the road.\nYour friends have chosen to turn right, what path do you choose? ")
+function startPlay () {
+    const shouldWePlay = prompt("Would you like to play Lewis' Life Lessons?");
+    if (shouldWePlay.toLowerCase() === "yes") {
+        console.log("Great, please make youre choice by typing either 'left' or 'right'")
+        theAdventure()
+    }else if (shouldWePlay.toLowerCase() === "no") {
+        console.log("That a shame, see you again soon")
+    } else {
+        console.log("Invalid input")
+        startPlay()
+    }
+}
+
+function theAdventure () {
+    const firstTurn = prompt("You are hiking Ben Nevis with some friends, and you come to a fork in the road.\nYour friends have chosen to turn right, what path do you choose? ").toLowerCase()
     if (firstTurn === "left") {
         console.log("Well you 'left' your troubles behind. You Win!")
     } else if (firstTurn === "right") {
         console.log("Sorry, you your friends 'right' off a cliff. You Lose :(")
     } else {
         console.log("Invalid input")
+        theAdventure()
     }
-}else if (shouldWePlay.toLowerCase() === "no") {
-    console.log("That a shame, see you again soon")
-} else {
-    console.log("invalid input")
 }
